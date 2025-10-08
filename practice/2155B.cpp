93d0f4a72e02f182c1eeb3f541b3bef9e5a19dc9
sanjith <sanjith@tutamail.com>
2025-10-08 22:54:59 +0530

    08-10-2025

---
+++ practice/2155B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define htii unordered_map<int, int>
+#define htci unordered_map<char, int>
+#define htil unordered_map<int, ll>
+#define htli unordered_map<ll, int>
+#define htcl unordered_map<char, ll>
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+#ifdef DEBUG
+    #define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+    #define dbg(x)
+#endif
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int k;
+    cin >> k;
+
+    k = n*n - k;
+
+    if (k == 1) {
+        cout << "NO\n";
+        return;
+    }
+
+    cout << "YES\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (k > 0) {
+                k -= 1;
+
+                if (i == 0 && j == 0) cout << 'R';
+                else if (i == 0) cout << 'L';
+                else cout << 'U';
+            } else {
+                cout << 'D';
+            }
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    FAST_IO
+    
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
