e17647c18c4ff1846d555fd5052f28420a41c5f5
sanjith <sanjith@tutamail.com>
2026-01-29 23:02:40 +0530

    random qn

---
+++ random/2084A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vc vector<char>
+#define vs vector<string>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define htii unordered_map<int, int>
+#define htci unordered_map<char, int>
+#define htil unordered_map<int, ll>
+#define htli unordered_map<ll, int>
+#define htcl unordered_map<char, ll>
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+#ifdef DEBUG
+    #define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+    #define dbg(x)
+#endif
+
+
+void solve() {
+    int n; cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "-1\n";
+        return;
+    }
+
+    cout << n;
+
+    for (int i = 1; i < n; ++i) {
+        cout << " " << i;
+    }
+
+    cout << endl;
+
+    return;
+}
+
+int main() {
+    FAST_IO
+    
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
