b90614ddac6eb09cd16cd4f531d5c085ada40e3b
sanjith <sanjith@tutamail.com>
2026-01-30 23:34:19 +0530

    ts broke me 💔😭

---
+++ random/2084B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vc vector<char>
+#define vs vector<string>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define htii unordered_map<int, int>
+#define htci unordered_map<char, int>
+#define htil unordered_map<int, ll>
+#define htli unordered_map<ll, int>
+#define htcl unordered_map<char, ll>
+
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+#ifdef DEBUG
+    #define dbg(x) cerr << #x << " = " << (x) << "\n"
+#else
+    #define dbg(x)
+#endif
+
+void solve() {
+    int n; cin >> n;
+
+    vll a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+	int p = min_element(a.begin(), a.end()) - a.begin();
+
+    ll g = 0;
+	
+    for (int i = 0; i < n; ++i) {
+		if (i != p && a[i] % a[p] == 0) {
+			g = __gcd(g, a[i]);
+		}
+	}
+	
+    cout << (g == a[p] ? "Yes\n" : "No\n");
+    return;
+}
+
+int main() {
+    FAST_IO
+    
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
