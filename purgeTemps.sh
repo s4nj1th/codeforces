b16bda4d610f3b7087681812acc1a401af4947a5
sanjith <sanjith@tutamail.com>
2026-02-11 00:00:41 +0530

    random qn for a yn

--- purgeTemps.sh
+++ purgeTemps.sh
@@ -2,11 +2,20 @@
 
 count=0
 exts="bin o out exe"
+pref=".cph"
 
 for e in $exts; do
   files=$(ls *.$e 2>/dev/null)
   for f in $files; do
-    rm -f "$f"
+    rm "$f"
+    count=$((count+1))
+  done
+done
+
+for e in $pref; do
+  files=$(ls $e* 2>/dev/null)
+  for f in $files; do
+    rm "$f"
     count=$((count+1))
   done
 done
